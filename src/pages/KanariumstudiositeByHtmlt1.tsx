23193bd38007c9bf67453c5266cb65cf03b6acd8
funakik <funakik>
2023-06-26 01:35:16 +0000

    Pushed from Locofy Builder

---
+++ src/pages/KanariumstudiositeByHtmlt1.tsx
@@ -0,0 +1,315 @@
+import { FunctionComponent } from "react";
+import styles from "./KanariumstudiositeByHtmlt1.module.css";
+const KanariumstudiositeByHtmlt1: FunctionComponent = () => {
+  return (
+    <div className={styles.kanariumstudiositeByHtmlt}>
+      <div className={styles.divappear}>
+        <div className={styles.section}>
+          <img
+            className={styles.s96x150F9a949977d4d467694Icon}
+            alt=""
+            src="/s96x150-f9a949977d4d46769415ab4b9ea4780asvg1.svg"
+          />
+          <div className={styles.div}>
+            <div className={styles.heading1}>
+              <div className={styles.kanarium}>kanarium</div>
+            </div>
+            <div className={styles.ptext}>
+              <div className={styles.web}>
+                <p className={styles.p}>はじめまして。</p>
+                <p className={styles.p}>オカメインコとのんびり暮らす</p>
+                <p className={styles.p}>フリーランスのWebデザイナーです。</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.section1}>
+          <div className={styles.heading2}>
+            <div className={styles.do}>Do</div>
+          </div>
+          <div className={styles.ui}>UIデザインを中心にしたデザインワーク</div>
+          <div className={styles.divappear1}>
+            <div className={styles.div1}>
+              <div className={styles.developerBoard}>developer_board</div>
+              <div className={styles.divappear2}>
+                <div className={styles.design}>Improvement</div>
+                <div className={styles.div2}>効果的なプランをご提案</div>
+              </div>
+            </div>
+            <div className={styles.div3}>
+              <div className={styles.developerBoard}>format_paint</div>
+              <div className={styles.divappear3}>
+                <div className={styles.heading12}>
+                  <div className={styles.design}>Design</div>
+                </div>
+                <div className={styles.div4}>要件 + 機能的なデザイン</div>
+              </div>
+            </div>
+            <div className={styles.div3}>
+              <div className={styles.developerBoard}>keyboard</div>
+              <div className={styles.divappear4}>
+                <div className={styles.heading13}>
+                  <div className={styles.design}>Coding</div>
+                </div>
+                <div className={styles.div6}>運用を意識したページ作成</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.divappear5}>
+            <div className={styles.heading14}>
+              対応範囲はご相談しながら必要な工程を選定します
+            </div>
+            <div className={styles.divappear6}>
+              <div className={styles.divappear7}>
+                <div className={styles.heading15}>
+                  <b className={styles.b}>準備</b>
+                </div>
+                <div className={styles.heading16}>
+                  <div className={styles.div7}>
+                    <p className={styles.p}>サービスの</p>
+                    <p className={styles.p}>理解/学習/分析</p>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.keyboardArrowRight}>
+                keyboard_arrow_right
+              </div>
+              <div className={styles.divappear8}>
+                <div className={styles.heading17}>
+                  <b className={styles.b}>計画</b>
+                </div>
+                <div className={styles.heading18}>
+                  <div className={styles.div7}>課題の洗出</div>
+                </div>
+                <div className={styles.heading19}>対応範囲の設定</div>
+              </div>
+              <div className={styles.keyboardArrowRight}>
+                keyboard_arrow_right
+              </div>
+              <div className={styles.divappear9}>
+                <div className={styles.heading110}>
+                  <b className={styles.b}>分析</b>
+                </div>
+                <div className={styles.heading111}>ペルソナ定義</div>
+                <div className={styles.heading112}>ジャーニーマップ作成</div>
+              </div>
+              <div className={styles.keyboardArrowRight}>
+                keyboard_arrow_right
+              </div>
+              <div className={styles.divappear10}>
+                <div className={styles.heading113}>
+                  <b className={styles.b}>実証</b>
+                </div>
+                <div className={styles.heading114}>ワイヤーフレーム作成</div>
+                <div className={styles.heading115}>プロトタイプ作成</div>
+                <div className={styles.heading116}>ユーザーテスト</div>
+                <div className={styles.heading117}>振り返り・改善</div>
+              </div>
+              <div className={styles.keyboardArrowRight}>
+                keyboard_arrow_right
+              </div>
+              <div className={styles.divappear11}>
+                <div className={styles.heading118}>
+                  <b className={styles.b}>実装</b>
+                </div>
+                <div className={styles.heading119}>ビジュアルデザイン</div>
+                <div className={styles.heading120}>HTML/CSSコーディング</div>
+                <div className={styles.heading121}>振り返り・改善・運用</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.ptext1}>
+            <div className={styles.lpstudio}>
+              ※新規のLP制作はstudioを利用したもののみ対応
+            </div>
+          </div>
+        </div>
+        <div className={styles.section2}>
+          <div className={styles.heading21}>
+            <div className={styles.do}>Works</div>
+          </div>
+          <div className={styles.div9}>携わった事例を掲載いたします</div>
+          <div className={styles.divappear12}>
+            <div className={styles.div10}>
+              <img className={styles.linkIcon} alt="" src="/link@2x.png" />
+              <div className={styles.divappear13}>
+                <div className={styles.link}>
+                  <div className={styles.mamaroGo}>mamaro GO</div>
+                  <div className={styles.ptext2}>
+                    <div className={styles.mamaroGoui}>
+                      <p className={styles.p}>
+                        授乳室検索アプリ mamaro
+                        GOお急ぎのUIデザイン刷新のお手伝いをさせて
+                      </p>
+                      <p className={styles.p}>
+                        いただきました。ご依頼内容はビジュアルデザインの刷新のみでしたがお
+                      </p>
+                      <p className={styles.p}>
+                        話ししていく中で出てきたUX改善のご提案強豪との差異なども共有させて
+                      </p>
+                      <p className={styles.p}>
+                        いただいています。身の回りの新生児育成経験のある人たちにヒアリング
+                      </p>
+                      <p className={styles.p}>をし、アプリ...</p>
+                    </div>
+                  </div>
+                  <div className={styles.trim}>Trim株式会社 様</div>
+                </div>
+                <div className={styles.link1}>
+                  <div className={styles.ui1}>UI</div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.div11}>
+              <img className={styles.linkIcon1} alt="" src="/link1@2x.png" />
+              <div className={styles.divappear14}>
+                <div className={styles.link}>
+                  <div className={styles.div12}>トラック簿</div>
+                  <div className={styles.ptext3}>
+                    <div className={styles.mamaroGoui}>
+                      <p className={styles.p}>
+                        倉庫管理を最適化倉庫で荷積み・降ろしを行うドライバー向け受付サービス
+                      </p>
+                      <p className={styles.p}>
+                        「トラック簿」手間のかかる配車・集車業務を効率化する「配車プラス」業
+                      </p>
+                      <p className={styles.p}>
+                        務リソースを最適化する「適材ナビ」のデザインを担当。ITリテラシーが高
+                      </p>
+                      <p className={styles.p}>
+                        くない利用者にも受け入れてもらえるようなUIを目指しました。トラック
+                      </p>
+                      <p className={styles.p}>簿</p>
+                    </div>
+                  </div>
+                  <div className={styles.div13}>株式会社モノフル 様</div>
+                </div>
+                <div className={styles.divappear15}>
+                  <div className={styles.link3}>
+                    <div className={styles.ui1}>UI</div>
+                  </div>
+                  <div className={styles.link4}>
+                    <div className={styles.ui1}>UX</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.link5}>
+            <div className={styles.ptext4}>
+              <div className={styles.more}>More</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.section3}>
+          <div className={styles.heading22}>
+            <div className={styles.do}>About</div>
+          </div>
+          <div className={styles.div14}>自己紹介をちょっぴり</div>
+          <img className={styles.pseudoIcon} alt="" src="/pseudo@2x.png" />
+          <div className={styles.divappear16}>
+            <div className={styles.ptext5}>
+              <div className={styles.div15}>
+                未来に向けた「今を最適化」するのが得意なデザイナーです。
+              </div>
+            </div>
+            <div className={styles.ptext6}>
+              <div className={styles.webI}>
+                <span className={styles.txt}>
+                  <p className={styles.p}>
+                    2017年よりフリーランスのWEBデザイナーをしています。それ以前は派
+                  </p>
+                  <p className={styles.p}>
+                    遣で制作会社やIT企業にて約10年ほどWEB制作を行ってきました。
+                  </p>
+                </span>
+              </div>
+              <div className={styles.gitsassscss}>
+                <span className={styles.txt}>
+                  <p className={styles.p}>
+                    開発に携わってきているのでGITなどのバージョン管理やSASS(SCSS)で
+                  </p>
+                  <p className={styles.p}>
+                    の制作などを採用されてる現場にも初期の学習コストが少なく参加でき
+                  </p>
+                  <p className={styles.p}>ます。</p>
+                </span>
+              </div>
+              <div className={styles.cssFramework}>
+                自作CSS Frameworkを育成中。
+              </div>
+              <div className={styles.strengthsfinder5}>
+                <span className={styles.txt}>
+                  <p className={styles.p}>
+                    StrengthsFinderで見る私の資質上位5位は
+                  </p>
+                  <p className={styles.p}>
+                    「慎重さ」「学習欲」「達成欲」「目標志向」「親密性」。
+                  </p>
+                </span>
+              </div>
+              <div className={styles.div16}>
+                <span className={styles.txt}>
+                  <p className={styles.p}>
+                    複雑に絡んだ物事を解いていくのを得意とします。控えめな人間ではあ
+                  </p>
+                  <p className={styles.p}>
+                    りますが指示待ちというわけではなく、自走して着実に、集中して目標
+                  </p>
+                  <p className={styles.p}>
+                    達成に向かいます。期待していただけると、単純な人間なのでがんばり
+                  </p>
+                  <p className={styles.p}>ます！</p>
+                </span>
+              </div>
+            </div>
+            <div className={styles.divappear17}>
+              <div className={styles.heading3}>
+                <div className={styles.div17}>屋号</div>
+              </div>
+              <div className={styles.ptext7}>
+                <div className={styles.web}>kanarium</div>
+              </div>
+            </div>
+            <div className={styles.divappear18}>
+              <div className={styles.heading31}>拠点</div>
+              <div className={styles.ptext8}>
+                <div className={styles.web}>東京</div>
+              </div>
+            </div>
+            <div className={styles.divappear19}>
+              <div className={styles.heading32}>事業内容</div>
+              <div className={styles.ptext9}>
+                <div className={styles.web}>
+                  <p className={styles.p}>UI/UXデザイン</p>
+                  <p className={styles.p}>HTML/CSSコーディング</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.footer}>
+          <div className={styles.heading23}>
+            <div className={styles.do}>Contact</div>
+          </div>
+          <div className={styles.divappear20}>
+            <div className={styles.link6}>
+              <img className={styles.icon} alt="" src="/icon2.svg" />
+            </div>
+            <div className={styles.link7}>
+              <img className={styles.icon1} alt="" src="/icon3.svg" />
+            </div>
+            <div className={styles.link8}>
+              <div className={styles.wantedy}>WANTEDY</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.link9}>
+        <img className={styles.bannerpngIcon} alt="" src="/bannerpng@2x.png" />
+      </div>
+    </div>
+  );
+};
+
+export default KanariumstudiositeByHtmlt1;
